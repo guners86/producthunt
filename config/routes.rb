27cf5ffde1327a6dab8baaeae9c22a5ad6297c85
guners86 <guners86@hotmail.com>
2015-09-05 14:14:38 -0500

    Se agrega la ruta /products/:id

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 	get '/products', to:'products#index'
 	get '/products/new', to: 'products#new'
 	post '/products', to: 'products#create'
+	get '/products/:id', to: 'products#show'
 
   	get '/welcome', to:'welcome#index'
   	
