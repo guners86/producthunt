d3050018d9308b52cf69b5bcf03492ad1844d0d1
guners86 <guners86@hotmail.com>
2015-09-06 14:24:00 -0500

    Se reemplaza la configuracion de las rutas del producto por un sesources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
 
-	get '/products', to:'products#index', as: :products
-	get '/products/new', to: 'products#new', as: :new_product
-	post '/products', to: 'products#create'
-	get '/products/:id', to: 'products#show'
-	get '/products/:id/edit', to: 'products#edit', as: :product
-	patch '/products/:id', to: 'products#update'
+	# get '/products', to:'products#index', as: :products
+	# get '/products/new', to: 'products#new', as: :new_product
+	# post '/products', to: 'products#create'
+	# get '/products/:id', to: 'products#show'
+	# get '/products/:id/edit', to: 'products#edit', as: :product
+	# patch '/products/:id', to: 'products#update'
+	# delete '/products/:id', to: 'products#destoy'
+
+	# Recurso que abstrae todas las configuraciones anteriores del producto
+	resources :products
+
+	# Configuracion para mostrar solo las rutas deseadas en este caso index y new
+	# resources :products, only: [:index, :new]
+
+	# Configuracion para mostrar todas las rutas excepto index y new
+	# vresources :products, except: [:index, :new]
 
   	get '/welcome', to:'welcome#index'
   	
