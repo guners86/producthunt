a8b3668f4a0d62f4ac72e9ef2485bf435935e31f
guners86 <guners86@hotmail.com>
2015-09-06 14:06:02 -0500

    Se crea un partial para el formulario del producto

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 	get '/products/new', to: 'products#new', as: :new_product
 	post '/products', to: 'products#create'
 	get '/products/:id', to: 'products#show'
-	get '/products/:id/edit', to: 'products#edit'
+	get '/products/:id/edit', to: 'products#edit', as: :product
 	patch '/products/:id', to: 'products#update'
 
   	get '/welcome', to:'welcome#index'
